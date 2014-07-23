90b7c898e0aae98441e2e2e1c09f8a9832b5e3d1
fahchen <dev.fah@gmail.com>
2014-07-23 21:51:39 +0800

    Add client

--- lib/nexmo/voice.rb
+++ lib/nexmo/voice.rb
@@ -5,5 +5,14 @@ require 'nexmo/voice/version'
 module Nexmo
   module Voice
     BASE_URL = 'https://rest.nexmo.com'
+
+    class Client
+      attr_accessor :api_key, :api_secret
+
+      def initialize(api_key, api_secret)
+        @api_key, @api_secret = api_key, api_secret
+      end
+    end
+
   end
 end
--- spec/nexmo/voice_spec.rb
+++ spec/nexmo/voice_spec.rb
@@ -5,3 +5,17 @@ RSpec.describe Nexmo::Voice do
     expect(Nexmo::Voice::BASE_URL).to eq 'https://rest.nexmo.com'
   end
 end
+
+RSpec.describe Nexmo::Voice::Client do
+  let(:api_key) { 'test_api_key' }
+  let(:api_secret) { 'test_api_secret' }
+  let(:client) { Nexmo::Voice::Client.new api_key, api_secret }
+
+  it 'sets the api_key' do
+    expect(client.api_key).to eq api_key
+  end
+
+  it 'sets the api_secret' do
+    expect(client.api_secret).to eq api_secret
+  end
+end
