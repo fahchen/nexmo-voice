2cd07227423560f71657b407fff6675b6af6bd26
fahchen <06fahchen@gmail.com>
2016-01-26 23:48:06 +0800

    Add timeout specs

--- spec/nexmo/voice/calls_spec.rb
+++ spec/nexmo/voice/calls_spec.rb
@@ -3,20 +3,28 @@ require 'spec_helper'
 describe Nexmo::Voice::Calls do
   let(:client) { Nexmo::Voice::Client.new('api_key', 'api_secret') }
   let(:entity) { { to: '8618000000001', answer_url: 'http://test.com/answer_url' } }
-
-  before do
-    response_json = {
+  let(:response_json) {
+    {
       'call-id' => 'd7d74aed5c754a992586c5baf01ed24d',
       'to' => '8618600000001',
       'status' => '0',
       'error-text' => 'Success'
     }
-    stub_request(:get, %r(https://api.nexmo.com/call/json.*)).to_return(body: response_json)
-  end
+  }
 
   it 'not raises errors' do
+    stub_request(:get, %r(https://api.nexmo.com/call/json.*)).to_return(body: response_json)
+
     expect {
       client.calls.create(entity)
     }.not_to raise_error
+  end
+
+  it 'timeouts' do
+    stub_request(:get, %r(https://api.nexmo.com/call/json.*)).to_timeout
+
+    expect {
+      client.calls.create(entity)
+    }.to raise_error
   end
 end
